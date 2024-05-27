9d3a44a5fcf75d77997342d25fe04e1bc03956f6
Michael Hegerhorst <michael.hegerhorst+github@gmail.com>
2024-05-26 20:16:15 -0600

    Allow CMake Build to configure the project if it is not already

--- lua/overseer/template/cmakeseer/cmake_build.lua
+++ lua/overseer/template/cmakeseer/cmake_build.lua
@@ -3,16 +3,35 @@ local function builder()
 	local build_dir = cwd .. "/build"
 
 	---@type overseer.TaskDefinition
-	return {
+	local task = {
 		name = "CMake Build",
-		cmd = "cmake -B " .. build_dir .. " -S " .. cwd .. " && cmake --build " .. build_dir,
+		cmd = "cmake --build " .. build_dir,
+		components = {
+			{
+				"unique",
+				restart_interrupts = false,
+			},
+			"default",
+		},
 	}
+
+	if vim.fn.filereadable(build_dir .. "/CMakeCache.txt") == 0 then
+		-- Insert just before "default" to minimize shifts
+		table.insert(task.components, #task.components - 1, {
+			"dependencies",
+			task_names = {
+				require("overseer.template.cmakeseer.cmake_configure").name,
+			},
+		})
+	end
+
+	return task
 end
 
 ---@type overseer.TemplateFileDefinition
 return {
 	name = "CMake Build",
-	desc = "Builds all  targets in the current CMake project",
+	desc = "Builds all targets in the current CMake project, configuring the project if it isn't already",
 	builder = builder,
 	condition = {
 		callback = function()
