85af60411e2ae57f713debf8c517f07af9881f99
Michael Hegerhorst <michael.hegerhorst+github@gmail.com>
2024-06-18 10:58:57 -0600

    Allow build before configure

--- lua/overseer/template/cmakeseer/cmake_build.lua
+++ lua/overseer/template/cmakeseer/cmake_build.lua
@@ -1,40 +1,39 @@
 local Cmakeseer = require("cmakeseer")
 
 local function builder()
-	---@type overseer.TaskDefinition
-	local task = {
-		name = "CMake Build",
-		cmd = Cmakeseer.get_build_command(),
-		components = {
-			{
-				"unique",
-				restart_interrupts = false,
-			},
-			"default",
-		},
-	}
+  ---@type overseer.TaskDefinition
+  local task = {
+    name = "CMake Build",
+    cmd = "cmake",
+    args = {
+      "--build",
+      Cmakeseer.get_build_directory(),
+    },
+    components = {
+      {
+        "unique",
+        restart_interrupts = false,
+      },
+      "default",
+    },
+  }
 
-	if not Cmakeseer.project_is_configured() then
-		-- Insert just before "default" to minimize shifts
-		table.insert(task.components, #task.components - 1, {
-			"dependencies",
-			task_names = {
-				require("overseer.template.cmakeseer.cmake_configure").name,
-			},
-		})
-	end
+  if not Cmakeseer.project_is_configured() then
+    -- Insert just before "default" to minimize shifts
+    table.insert(task.components, #task.components - 1, {
+      "dependencies",
+      task_names = {
+        require("overseer.template.cmakeseer.cmake_configure").name,
+      },
+    })
+  end
 
-	return task
+  return task
 end
 
 ---@type overseer.TemplateFileDefinition
 return {
-	name = "CMake Build",
-	desc = "Builds all targets in the current CMake project, configuring the project if it isn't already",
-	builder = builder,
-	condition = {
-		callback = function()
-			return vim.fn.filereadable(vim.fn.getcwd() .. "/CMakeLists.txt") ~= 0
-		end,
-	},
+  name = "CMake Build",
+  desc = "Builds all targets in the current CMake project, configuring the project if it isn't already",
+  builder = builder,
 }
--- lua/overseer/template/cmakeseer/cmake_clean.lua
+++ lua/overseer/template/cmakeseer/cmake_clean.lua
@@ -1,27 +1,25 @@
 local Cmakeseer = require("cmakeseer")
 
 local function builder()
-	---@type overseer.TaskDefinition
-	return {
-		name = "CMake Clean",
-		cmd = "cmake",
-		args = {
-			"--build",
-			Cmakeseer.get_build_directory(),
-			"--target",
-			"clean",
-		},
-	}
+  ---@type overseer.TaskDefinition
+  return {
+    name = "CMake Clean",
+    cmd = "cmake",
+    args = {
+      "--build",
+      Cmakeseer.get_build_directory(),
+      "--target",
+      "clean",
+    },
+  }
 end
 
 ---@type overseer.TemplateFileDefinition
 return {
-	name = "CMake Clean",
-	desc = "Cleans the CMake build directory",
-	builder = builder,
-	condition = {
-		callback = function()
-			return Cmakeseer.project_is_configured()
-		end,
-	},
+  name = "CMake Clean",
+  desc = "Cleans the CMake build directory",
+  builder = builder,
+  condition = {
+    callback = Cmakeseer.project_is_configured,
+  },
 }
