9b79d1d7a141028f7f09d01d4bd1ffe5d3ebdda1
Michael Hegerhorst <michael.hegerhorst+work@gmail.com>
2025-11-24 15:59:59 -0700

    Start using OOP

--- lua/cmakeseer/neotest/gtest/suite.lua
+++ lua/cmakeseer/neotest/gtest/suite.lua
@@ -1,17 +1,72 @@
 ---@class cmakeseer.neotest.gtest.suite.Basic A suite with no additional parameters or values. They have no prefix nor postfix.
 ---@field name string The name of the suite.
 ---@field tests table<string> A set of the tests in the suite.
+local Basic = {
+  name = "Unknown Suite",
+  tests = {},
+}
+
+---@param o nil|table Optional table containing initial states.
+---@return cmakeseer.neotest.gtest.suite.Basic instance A new Basic instance.
+function Basic:new(o)
+  o = o or {}
+  o.tests = o.tests or {}
+  setmetatable(o, self)
+  self.__index = self
+  return o
+end
 
 ---@class cmakeseer.neotest.gtest.suite.Parameterized: cmakeseer.neotest.gtest.suite.Basic A suite with parameterized arguments. They have a prefix, but the postfix is added to the individual tests instead.
 ---@field value_parameters table<string, string[]> Prefixes to value parameter IDs to values. The postfix of each test can identify the parameter used for each prefix.
+local Parameterized = Basic:new({
+  value_parameters = {},
+})
+
+---@param o nil|table Optional table containing initial states.
+---@return cmakeseer.neotest.gtest.suite.Parameterized instance A new Parameterized instance.
+function Parameterized:new(o)
+  ---@class cmakeseer.neotest.gtest.suite.Parameterized
+  o = Basic.new(self, o)
+  o.value_parameters = o.value_parameters or {}
+  return o
+end
 
 ---@class cmakeseer.neotest.gtest.suite.Typed: cmakeseer.neotest.gtest.suite.Basic A suite with using a known set of types. They have a postfix, but no prefix.
 ---@field type_parameters string[] Type parameter IDs to types. The postfix can identify the parameter.
+local Typed = Basic:new({
+  type_parameters = {},
+})
+
+---@param o nil|table Optional table containing initial states.
+---@return cmakeseer.neotest.gtest.suite.Typed instance A new Typed instance.
+function Typed:new(o)
+  ---@class cmakeseer.neotest.gtest.suite.Typed
+  o = Basic.new(self, o)
+  o.type_parameters = o.type_parameters or {}
+  return o
+end
 
 ---@class cmakeseer.neotest.gtest.suite.ParameterizedTyped: cmakeseer.neotest.gtest.suite.Basic A suite with parameterized types. The have a prefix and a postfix.
 ---@field parameterized_type_parameters table<string, string[]> Prefixes to postfixes to type parameters.
+local ParameterizedTyped = Basic:new({
+  parameterized_type_parameters = {},
+})
 
-local M = {}
+---@param o nil|table Optional table containing initial states.
+---@return cmakeseer.neotest.gtest.suite.ParameterizedTyped instance A new Typed instance.
+function ParameterizedTyped:new(o)
+  ---@class cmakeseer.neotest.gtest.suite.ParameterizedTyped
+  o = Basic.new(self, o)
+  o.type_parameters = o.type_parameters or {}
+  return o
+end
+
+local M = {
+  Basic = Basic,
+  Parameterized = Parameterized,
+  Typed = Typed,
+  ParameterizedTyped = ParameterizedTyped,
+}
 
 --- Identifies a suite type just from its ID parts.
 ---@param prefix string? The prefix of the suite.
