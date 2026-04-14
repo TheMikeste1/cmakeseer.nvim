e5a62ee2e745315dada31ade9cc307aa61d5152a
Michael Hegerhorst <michael.hegerhorst@gmail.com>
2026-04-13 21:23:47 -0600

    Fix cache variable trying to call variable

--- lua/cmakeseer/cmake/cache/variable.lua
+++ lua/cmakeseer/cmake/cache/variable.lua
@@ -11,23 +11,23 @@ local Variable = {}
 --- @param name string The name of the variable.
 --- @param value string The value of the variable.
 --- @param description string A description for the variable.
---- @param type string The type of the variable.
+--- @param type_ string The type of the variable.
 --- @param advanced boolean? Whether the variable is advanced.
 --- @return cmakeseer.cmake.cache.Variable
-function Variable:new(name, value, description, type, advanced)
+function Variable:new(name, value, description, type_, advanced)
   vim.validate("name", name, function(v)
     return type(v) == "string" and v ~= ""
   end, false, "non-empty string")
   vim.validate("description", description, "string", false)
   vim.validate("value", value, "string", false)
-  vim.validate("type", type, "string", false)
+  vim.validate("type", type_, "string", false)
   vim.validate("advanced", advanced, "boolean", true)
 
   local new_var = {}
   new_var.name = name
   new_var.description = description
   new_var.value = value
-  new_var.type = type
+  new_var.type = type_
   new_var.advanced = advanced ~= nil and advanced or false
   setmetatable(new_var, self)
   self.__index = self
