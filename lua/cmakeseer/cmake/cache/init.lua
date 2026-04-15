4eba96c0c834e6981e526d252cf83a9bf34955e0
Michael Hegerhorst <michael.hegerhorst@gmail.com>
2026-04-14 20:27:17 -0600

    Work on screen

--- lua/cmakeseer/cmake/cache/init.lua
+++ lua/cmakeseer/cmake/cache/init.lua
@@ -5,22 +5,14 @@ local M = {}
 --- Processes a single line from the CMake cache file, updating the provided variable table and description history.
 --- @param vars table<string, cmakeseer.cmake.cache.Variable> The table to store and update cache variables.
 --- @param working_description string[] A mutable array holding the current description pieces accumulated from comment lines. Will be emptied.
+--- @param waiting_to_apply_advanced table<string> A set of advanced variables that have yet to be applied.
 --- @param line string The raw string representing the cache variable line to process.
-local function process_cache_line(vars, working_description, line)
+local function process_cache_line(vars, working_description, waiting_to_apply_advanced, line)
   local var = M.parse_cache_variable(line)
   if var == nil then
     vim.notify("Invalid cache item: " .. line, vim.log.levels.ERROR)
     return
   end
-
-  local description = table.concat(working_description)
-  -- Clear working_description
-  for i = #working_description, 1, -1 do
-    working_description[i] = nil
-  end
-
-  var.description = description
-  vars[var.name] = var
 
   if vim.endswith(var.name, "-ADVANCED") then
     local target_var_name = var.name:sub(1, #var.name - #"-ADVANCED")
@@ -28,8 +20,22 @@ local function process_cache_line(vars, working_description, line)
     if target_var ~= nil then
       target_var.advanced = true
     else
-      vim.notify(string.format("Cache variable `%s` is marked advanced, but does not exist??", target_var_name), vim.log.levels.WARN)
+      waiting_to_apply_advanced[target_var_name] = true
     end
+  else
+    if waiting_to_apply_advanced[var.name] then
+      var.advanced = true
+      waiting_to_apply_advanced[var.name] = nil
+    end
+
+    local description = table.concat(working_description)
+    var.description = description
+    vars[var.name] = var
+  end
+
+  -- Clear working_description
+  for i = #working_description, 1, -1 do
+    working_description[i] = nil
   end
 end
 
@@ -73,13 +79,18 @@ function M.parse_cache_string(cache_string)
     return not vim.startswith(line, "#") and line ~= ""
   end)
   local working_description = {}
+  local waiting_to_apply_advanced = {} -- Variables may or may not appear before their advanced indicator
   for line in lines do
     ---@cast line string
     if vim.startswith(line, "//") then
       table.insert(working_description, line:sub(3))
     else
-      process_cache_line(vars, working_description, line)
+      process_cache_line(vars, working_description, waiting_to_apply_advanced, line)
     end
+  end
+
+  for var, _ in pairs(waiting_to_apply_advanced) do
+    vim.notify(string.format("Cache variable `%s` is marked advanced, but does not exist??", var), vim.log.levels.WARN)
   end
 
   return vars
--- lua/cmakeseer/ui/edit_cache_screen.lua
+++ lua/cmakeseer/ui/edit_cache_screen.lua
@@ -1,5 +1,21 @@
-local M = {}
+local function pairsByKeys(t, f)
+  local a = {}
+  for n in pairs(t) do
+    table.insert(a, n)
+  end
+  table.sort(a, f)
+  local i = 0
+  return function()
+    i = i + 1
+    if a[i] == nil then
+      return nil
+    else
+      return a[i], t[a[i]]
+    end
+  end
+end
 
+local M = {}
 function M.edit_cache_screen()
   local cmakeseer = require("cmakeseer")
   if not cmakeseer.project_is_configured() then
@@ -24,11 +40,28 @@ function M.edit_cache_screen()
   local ns = vim.api.nvim_create_namespace("cmakeseer")
 
   -- TODO: Might be better to do this all at once
-  vim.iter(maybe_cache):enumerate():each(function(i, _, var)
-    vim.api.nvim_buf_set_lines(bufnr, i - 1, i - 1, false, { var.value })
+  local values = {}
+  local names = {}
+  local max_name_length = 0
+  vim
+    .iter(pairsByKeys(maybe_cache))
+    :filter(function(_, var)
+      ---@cast var cmakeseer.cmake.cache.Variable
+      return var.type ~= "STATIC" and var.type ~= "INTERNAL"
+    end)
+    :each(function(_, var)
+      table.insert(values, var.value)
+      table.insert(names, var.name)
+      max_name_length = math.max(max_name_length, #var.name)
+    end)
+
+  vim.api.nvim_buf_set_lines(bufnr, 0, #values - 1, false, values)
+  vim.iter(names):enumerate():each(function(i, name)
+    local padded_name = string.format("%-" .. max_name_length .. "s", name)
     vim.api.nvim_buf_set_extmark(bufnr, ns, i - 1, 0, {
-      virt_text = { { var.name .. " | ", "Comment" } },
+      virt_text = { { padded_name, "Comment" }, { " | ", "Comment" } },
       virt_text_pos = "inline",
+      right_gravity = false,
     })
   end)
 end
