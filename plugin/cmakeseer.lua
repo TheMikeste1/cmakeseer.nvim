e69d2839cf9b5739aa60ac27c78ffeaad94f33c9
Michael Hegerhorst <michael.hegerhorst@gmail.com>
2026-04-21 22:01:25 -0600

    Add CMakeSeer command

--- plugin/cmakeseer.lua
+++ plugin/cmakeseer.lua
@@ -18,3 +18,35 @@ if CMakeSeer.project_is_configured() then
 end
 
 require("cmakeseer.dap").setup()
+local function handle_api_command(opts)
+  vim.notify("CMakeSeer opts: " .. vim.inspect(opts), vim.log.levels.DEBUG)
+  if not opts.fargs then
+    vim.notify("Missing args")
+    return
+  end
+
+  if opts.fargs[1] == "select_kit" then
+    require("cmakeseer").select_kit()
+    return
+  end
+
+  if opts.fargs[1] == "select_variant" then
+    require("cmakeseer").select_variant()
+    return
+  end
+
+  if opts.fargs[1] == "edit_cache_entry" then
+    require("cmakeseer.ui.edit_cache_entry")()
+    return
+  end
+
+  vim.notify("Unknown CMakeSeer command: " .. opts.args)
+end
+
+vim.api.nvim_create_user_command("CMakeSeer", handle_api_command, {
+  desc = "Access the CMakeSeer API",
+  nargs = "*",
+  complete = function(_, _)
+    return { "select_kit", "select_variant", "edit_cache_entry" }
+  end,
+})
